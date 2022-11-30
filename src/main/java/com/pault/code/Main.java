914d2e148c602d0f72438e28d85ce9f9c18e51ae
Paul Thomas <paulthomas2107@gmail.com>
2022-11-30 21:08:57 +0000

    More....

--- src/main/java/com/pault/code/Main.java
+++ src/main/java/com/pault/code/Main.java
@@ -3,10 +3,7 @@ package com.pault.code;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,7 @@ public class Main {
     public Main(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
+
     public static void main(String[] args) {
 
         log.info("Hello Paul !");
@@ -28,12 +26,24 @@ public class Main {
     }
 
 
-
     @GetMapping
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
 
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
     @GetMapping("/greet")
     public GreetResponse greet() {
         return new GreetResponse("Hi...Paul", List.of("Java", "Python", "C++"), new Person("Rory", 19, -30_000.12));
@@ -47,4 +57,9 @@ public class Main {
             List<String> faveProgrammingLanguages,
             Person person) {
     }
+
+    record NewCustomerRequest(String name, String email, Integer age) {
+
+    }
 }
+
