35faa8f4e717e4ae86e0cdb1c805545c02b6c825
Paul Thomas <paulthomas2107@gmail.com>
2022-11-30 20:42:35 +0000

    More....

--- src/main/java/com/pault/code/Main.java
+++ src/main/java/com/pault/code/Main.java
@@ -4,6 +4,8 @@ import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,11 +13,25 @@ import java.util.List;
 @Log
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
 
         log.info("Hello Paul !");
         SpringApplication.run(Main.class, args);
+    }
+
+
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
     @GetMapping("/greet")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT into customer (id, name, email, age) values (nextval('customer_id_sequence'),'Rory Anthony', 'rory@thomas.com', 19);
+INSERT into customer (id, name, email, age) values (nextval('customer_id_sequence'),'Caitlin Alexandra', 'cat@thomas.com', 22);
+INSERT into customer (id, name, email, age) values (nextval('customer_id_sequence'),'Paul', 'paul@thomas.com', 37);
+
+
+
